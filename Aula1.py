6de2dd1ccc2cd4cf4ad8d525d7aa6e5d767880c7
Fabiokleber1979 <fabiokleber@hotmail.com>
2026-01-11 14:24:08 -0300

    teste do git

--- Aula1.py
+++ Aula1.py
@@ -7,3 +7,19 @@ fruta_citrica = "MACA"
 # imprimindo minha variavel
 print (fruta_citrica)
 
+
+
+git config --global user.name "Fabiokleber1979"
+
+git config --global user.email "fabiokleber@hotmail.com"
+
+
+git add .
+git commit  -m " comentario responsivo"
+git push origin main
+
+
+
+link 
+
+https://github.com/Fabiokleber1979/Labs-Aws-IA
